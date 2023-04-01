5da6ec693d825b1652ffc14398ce7bbc456a5e2a
Abdullah-Ahmed1 <abdullah.ahmed10001@gmail.com>
2023-04-01 12:27:07 +0500

    product reducers added

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,20 @@
+import { ActionTypes } from "../constants/actionTypes"
+
+const initialState={
+    products : [
+        {
+            id : 1,
+            title : "Abdullah Ahmed",
+            category : "Programmer"
+        }
+    ]
+}
+
+export const productReducer = (state,action)=>{
+    switch(action.type){
+        case ActionTypes.SET_PRODUCTS:
+            return state
+        default:
+            return state    
+    }
+}
